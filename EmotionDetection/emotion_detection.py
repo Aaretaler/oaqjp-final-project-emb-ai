e463c340409812ca712c89c3d302ad7dd27a3e5d
Reto Kunz <retokunz@gmx.net>
2025-12-04 11:22:36 -0500

    finished project

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,19 +6,20 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json=myobj, headers=header)
-    print(response.text)    # TODO remove
     formatted_response = json.loads(response.text)
 
-    emotion_dict = formatted_response["emotionPredictions"][0]["emotion"]
-    anger_score = emotion_dict["anger"]
-    disgust_score = emotion_dict["disgust"]
-    fear_score = emotion_dict["fear"]
-    joy_score = emotion_dict["joy"]
-    sadness_score = emotion_dict["sadness"]
+    return_dict = {}
+    if response.status_code == 200:
+        emotion_dict = formatted_response["emotionPredictions"][0]["emotion"]
+        anger_score = emotion_dict["anger"]
+        disgust_score = emotion_dict["disgust"]
+        fear_score = emotion_dict["fear"]
+        joy_score = emotion_dict["joy"]
+        sadness_score = emotion_dict["sadness"]
 
-    dominant_emotion = max(emotion_dict, key=emotion_dict.get)
-
-    return {
+        dominant_emotion = max(emotion_dict, key=emotion_dict.get)
+        
+        return_dict = {
             'anger': anger_score,
             'disgust': disgust_score,
             'fear': fear_score,
@@ -26,3 +27,14 @@ def emotion_detector(text_to_analyse):
             'sadness': sadness_score,
             'dominant_emotion': dominant_emotion
             }
+    elif response.status_code == 400:
+        return_dict = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+            }
+
+    return return_dict
